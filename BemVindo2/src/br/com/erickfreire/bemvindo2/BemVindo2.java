7950b1b5ab64e89e176ede9c2b363400182c6864
Erick Freire <erilefr@gmail.com>
2022-10-01 18:52:51 -0300

    a
    a

---
+++ BemVindo2/src/br/com/erickfreire/bemvindo2/BemVindo2.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.bemvindo2;
+
+/**
+ * Bem Vindo a Programação em Java - Versão 2
+ * @author Erick Freire
+ * @version 1 - Criado em 01-10-2022
+ */
+
+public class BemVindo2 {
+
+	public static void main(String[] args) {
+		System.out.print("Bem Vindo a ");
+		System.out.println("Programação em Java!");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Bem_Vindo2
  Bem Vindo a Programação em Java - Versão 2
+
+  ## Imagem
+![Currículo no Computador](bem_vindo2.png)
